c76da82403711e5f8db5668fdfa27fce35e9c5dd
xmilygit <xmilygame@163.com>
2019-09-24 02:20:24 +0800

    正式配置

--- cfg/wechatconfig.js
+++ cfg/wechatconfig.js
@@ -1,16 +1,16 @@
 //正式配置
-// var wechatauth = {
-//     appid: 'wx4e22ba98362ec444',
-//     appsecret: '456d2b7769f5e91cd208a8399964d53f',
-//     token: 'xmilyhh'
-// }
+var wechatauth = {
+    appid: 'wx4e22ba98362ec444',
+    appsecret: '456d2b7769f5e91cd208a8399964d53f',
+    token: 'xmilyhh'
+}
 
 // 测试号配置
-var wechatauth = {
-    appid: 'wxba8db6584881bbab',
-    appsecret: '7d234a1b76eb803e6683d3a8945985bb',
-    token: 'xmilyhh'
-};
+// var wechatauth = {
+//     appid: 'wxba8db6584881bbab',
+//     appsecret: '7d234a1b76eb803e6683d3a8945985bb',
+//     token: 'xmilyhh'
+// };
 //MXThink测试用
 /*
 var wechatauth={
